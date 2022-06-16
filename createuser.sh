6033d8df09ae0acdf18dd8b040b914d4b1b8eecd
Matt Schell <mschell@doitt.nyc.gov>
2022-06-16 15:17:49 -0400

    corrects run-user-tests.sh on Azure, resolves #1

--- createuser.sh
+++ createuser.sh
@@ -8,6 +8,12 @@ psql -t -c "grant usage on schema sde to $TARGETUSER;"
 psql -t -c "grant select, insert, update, delete on public.geometry_columns TO $TARGETUSER;"
 psql -t -c "grant select, insert, update, delete on public.geography_columns TO $TARGETUSER;"
 psql -t -c "grant select on spatial_ref_sys to $TARGETUSER;"
-export PGUSER=$TARGETUSER
+if [[ $PGUSER == *"@"* ]]; then
+   #cut on the at sign, take second part
+   host=$(cut -d @ -f 2 <<< "$PGUSER")
+   export PGUSER="$TARGETUSER@$host"
+else
+    export PGUSER=$TARGETUSER
+fi
 export PGPASSWORD=$TARGETPASSWORD
 ./src/test/run-user-tests.sh 
